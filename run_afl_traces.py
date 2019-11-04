4f44a1486c59ae753e669b2986239e571fbe8305
Jiajun-Deng <demidengonly@gmail>
2019-11-03 19:45:54 -0600

    afl block works. Next: zzuf

--- run_afl_traces.py
+++ run_afl_traces.py
@@ -1,10 +1,7 @@
 import os
 import multiprocessing
 import datetime
-import shutil
-
-from utils.Config.Config import Config
-from utils.GetFunctions.GetFunctions import get_range
+import utils.Operations.Operations as op
 
 '''
 This tool runs AFL with multiprocessing on given program
@@ -15,43 +12,37 @@ This tool uses config options:
   afl target option
 '''
 
-def run_afl(indexlist, cores, timeout, afl_output_path, program_path, target_relative_path, afl_target_option):
+def run_afl(indexlist, cores, timeout, test_path, program_path,seed_path, afl_target_option):
     pool= multiprocessing.Pool(cores)
-    pool.map(run_afl_subprocess, (indexlist,timeout, afl_output_path, program_path, target_relative_path, afl_target_option))
+    pool.map(run_afl_subprocess, (indexlist, timeout, test_path, program_path,seed_path, afl_target_option))
     pool.close()
     pool.join()
     return
 
-def run_afl_subprocess(index,timeout, afl_output_path, program_path, target_relative_path, afl_target_option):
-    arg = "AFL_USE_ASAN=1 timeout %s afl-fuzz -m none -i %s -o %s %s/%s %s" % (timeout, "%s/afl_outcomes/afl_in" % afl_output_path, "%s/afl_outcomes/afl_out_%d" % (afl_output_path ,index), program_path, target_relative_path, afl_target_option)
+def run_afl_subprocess(core_index,timeout, test_path, program_path,seed_path, afl_target_option):
+
+    afl_output_path = "%s/afl_outcomes_%d" % (test_path ,core_index)
+    
+    op.mkdir(afl_output_path)
+    
+    arg = "AFL_USE_ASAN=1 timeout %s afl-fuzz -m none -i %s -o %s %s %s" % (timeout, seed_path, afl_output_path, program_path, afl_target_option)
     print(arg)
-    with open("%s/afl_outcomes/start_time_%d" % (afl_output_path, index), "w+") as st:
+
+    #Record the start time of a process
+    with open("%s/start_time_%d" % (afl_output_path, core_index), "w+") as st:
         st.write(str(datetime.datetime.now()))
         
     afl_pipe = os.popen(arg)
     afl_pipe.read()
     afl_pipe.close()
-    print("AFL run #%d has ended" % index)
+    print("AFL run #%d has ended" % core_index)
 
-if __name__ == '__main__':
-    lower, upper = get_range()
-    
-    config = Config()
-
-    global timeout 
-    timeout = config.get_config("timeout")
-
-    global afl_output_path
-    afl_output_path = config.get_afl_output_path()
-    
-    global program_path
-    program_path = config.get_config("afl program path")
-
-    global target_relative_path
-    target_relative_path = config.get_config("target relative path")
-
-    global afl_target_option
-    afl_target_option = config.get_afl_target_option()
-    
-    indexlist = list(range(lower, upper+1))
-    run_afl(indexlist, upper-lower+1, timeout, afl_output_path, program_path, target_relative_path, afl_target_option)
+def parallel_run_afl (indexlist, cores, timeout, test_path, program_path,seed_path, afl_target_option):
+    pool= multiprocessing.Pool(cores)
+    try:
+        for i in indexlist:
+            pool.apply_async(func = run_afl_subprocess, args = (i,timeout, test_path, program_path,seed_path, afl_target_option,))
+    except Exception as e:
+        print(e)
+    pool.close()
+    pool.join()
--- run_fuzzer.py
+++ run_fuzzer.py
@@ -7,67 +7,65 @@ from utils.GetFunctions.GetFunctions import get_range
      
 if __name__ == "__main__":
   
-  fuzzer_name = input("Choose the fuzzer: afl, radamsa, zzuf:")
-  
-  if fuzzer_name == "radamsa":
-      print("Fuzzing and testing of radamsa begins:")
-      
-      lower, upper = get_range()
-      config = Config()
-      index_list = list(range(lower,upper))#Decide which cores to use.
-      
-      #global variable is used by all processes.
-      global timeout
-      timeout = config.get_config("timeout")
-      timeout = int(timeout)
-      
-      global fuzzer_path
-      fuzzer_path = config.get_config("fuzzer_path")
-      
-      global seed_path
-      seed_path = config.get_config("seed_path")
-      
-      global test_path
-      test_path = config.get_config("test_path")
-      output_path = "%s/out" % (test_path)
-      err_path = "%s/err" % (test_path)
-      
-      global mutation_num
-      mutation_num = config.get_config("mutation_num")
-      mutation_num = int(mutation_num)
+    fuzzer_name = input("Choose the fuzzer: afl, radamsa, zzuf:")
+    global timeout
     
-      global program_path
-      program_path = config.get_config("program_path")
-      
-      ra.parallel_run_radamsa_with_timeout(index_list, upper-lower+1, test_path, program_path, fuzzer_path, seed_path, mutation_num, timeout)
-      
-  elif fuzzer_name == "afl":
-  
-    lower, upper = get_range()
-    
-    config = Config()
+    if fuzzer_name == "radamsa":
+        print("Fuzzing and testing of radamsa begins:")
         
-    indexlist = list(range(lower, upper+1))
+        lower, upper = get_range()
+        config = Config()
+        index_list = list(range(lower,upper))#Decide which cores to use.
+        
+        #global variable is used by all processes.
 
-    global timeout 
-    timeout = config.get_config("timeout")
-
-    global afl_output_path
-    afl_output_path = config.get_afl_output_path()
+        timeout = config.get_config("timeout")
+        timeout = int(timeout)
+        
+        global fuzzer_path
+        fuzzer_path = config.get_config("fuzzer path")
+        
+        global seed_path
+        seed_path = config.get_config("seed path")
+        
+        global test_path
+        test_path = config.get_config("test path")
+        output_path = "%s/out" % (test_path)
+        err_path = "%s/err" % (test_path)
+        
+        global mutation_num
+        mutation_num = config.get_config("mutation num")
+        mutation_num = int(mutation_num)
+      
+        global program_path
+        program_path = config.get_config("program path")
+        
+        ra.parallel_run_radamsa_with_timeout(index_list, upper-lower+1, test_path, program_path, fuzzer_path, seed_path, mutation_num, timeout)
+      
+    elif fuzzer_name == "afl":
+  
+        lower, upper = get_range()
+        
+        config = Config()
+            
+        indexlist = list(range(lower, upper+1))
     
-    global program_path
-    program_path = config.get_config("afl program path")
-
-    global target_relative_path
-    target_relative_path = config.get_config("target relative path")
-
-    global afl_target_option
-    afl_target_option = config.get_afl_target_option()
-
-    rafl.run_afl(indexlist, upper-lower+1)
+        #global timeout 
+        timeout = config.get_config("timeout")
+    
+        test_path = config.get_config("test path")
+        
+        program_path = config.get_config("program path")
+        
+        seed_path = config.get_config("seed path")
+    
+        global afl_target_option
+        afl_target_option = config.get_afl_target_option()
+    
+        rafl.parallel_run_afl(indexlist, upper-lower+1, timeout, test_path, program_path,seed_path, afl_target_option)
 
       
-  elif fuzzer_name == "zzuf":
-      print("TODO")
-  else:
-      print("TODO MORE FUZZERS")
+    elif fuzzer_name == "zzuf":
+        print("TODO")
+    else:
+        print("TODO MORE FUZZERS")
